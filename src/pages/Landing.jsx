d24b118a00fa1d673d42e053f071aad31e9da323
Nicolas Mayorga <namayorgav@gmail.com>
2023-09-19 21:45:52 -0500

    chore: fix the orbit circle animation.

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,18 @@
 function Landing() {
-  return <div>Landing</div>
+  return (
+    <section className="landing-spinner">
+      <div className="center">
+        <ul className="inner ">
+          <li className="outer "></li>
+          <li className="outer "></li>
+          <li className="outer "></li>
+          <li className="outer "></li>
+          <li className="outer "></li>
+          <li className="outer "></li>
+        </ul>
+      </div>
+      <h1>Cool round</h1>
+    </section>
+  )
 }
 export default Landing

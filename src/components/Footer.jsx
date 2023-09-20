cf9eb9f4dc1c47a72371f36e5e453292c18dbfce
Nicolas Mayorga <namayorgav@gmail.com>
2023-09-19 19:24:03 -0500

    styles: HomeLayout styled.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,28 @@
 function Footer() {
-  return <div>footer</div>
+  return (
+    <footer className="footer">
+      <p className="footer-desc">
+        Built by{" "}
+        <a
+          className="footer-link"
+          href="https://www.linkedin.com/in/nicol%C3%A1s-mayorga-7z0/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nicolas Mayorga
+        </a>
+        . The source code is on{" "}
+        <a
+          className="footer-link"
+          href="https://github.com/Git-Darkmoon/Mixology.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </p>
+    </footer>
+  )
 }
 export default Footer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,18 @@
+import { NavLink } from "react-router-dom"
+
 function Navbar() {
-  return <div>navbar</div>
+  return (
+    <nav className="navbar">
+      <div className="logo">Mixology</div>
+      <ul className="nav-links">
+        <NavLink to={"/"} className={"nav-link"}>
+          Home
+        </NavLink>
+        <NavLink to={"/about"} className={"nav-link"}>
+          About
+        </NavLink>
+      </ul>
+    </nav>
+  )
 }
 export default Navbar

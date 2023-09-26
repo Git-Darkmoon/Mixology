4006b752499b38e6edea350d0e26359eba143f20
Nicolas Mayorga <namayorgav@gmail.com>
2023-09-25 22:55:09 -0500

    feat: stale time on query.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import { Landing, About, Cocktail, Error, HomeLayout } from "./pages"
 
+import { loader as landingLoader } from "./pages/Landing"
+import { QueryClient } from "@tanstack/react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
-      { index: true, element: <Landing /> },
+      { index: true, element: <Landing />, loader: landingLoader(queryClient) },
       {
         path: "cocktail/:id",
         element: <Cocktail />,

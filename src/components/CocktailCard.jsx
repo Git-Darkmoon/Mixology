0f8f1dbbdec43f34094ac6fd825532485e417e5b
Nicolas Mayorga <namayorgav@gmail.com>
2023-09-26 21:39:54 -0500

    styles: changed layout of the card.

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom"
 function CocktailCard({ id, name, image }) {
   return (
     <article className="cocktail-card">
-      <h1 className="cocktail-name">{name}</h1>
-      <img src={image} alt={name} />
+      <picture>
+        <img src={image} alt={name} className="cocktail-image" />
+      </picture>
       <Link to={`/cocktail/${id}`} className="cocktail-btn">
-        details
+        Details
       </Link>
+      <h1 className="cocktail-name">{name}</h1>
     </article>
   )
 }

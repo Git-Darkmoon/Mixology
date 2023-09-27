d6931628c47151407577a35c5c6ae6b24b21211e
Nicolas Mayorga <namayorgav@gmail.com>
2023-09-26 21:39:08 -0500

    feat: dynamic orbiting cocktails.

---
+++ src/orbitCocktails.js
@@ -0,0 +1,27 @@
+export const classicCocktails = [
+  "Martini",
+  "Negroni",
+  "Mojito",
+  "Daiquiri",
+  "Manhattan",
+]
+export const tropicalCocktails = [
+  "MaiTai",
+  "PiñaColada",
+  "Zombie",
+  "Mojito",
+  "Hurricane",
+]
+export const modernCocktails = [
+  "Cosmopolitan",
+  "Margarita",
+  "OldFashioned",
+  "WhiskeySour",
+  "MoscowMule",
+]
+export const otherCocktails = [
+  "Mauresque",
+  "Zinger",
+  "BoraBora",
+  "VodkaRussian",
+]

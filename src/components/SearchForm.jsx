57de41e2baef88bbf8970b126a86b54d72d0020b
Nicolas Mayorga <namayorgav@gmail.com>
2023-09-26 20:37:40 -0500

    feat: Form component from React-Router for search.

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,9 +1,20 @@
-function SearchForm() {
+import { Form } from "react-router-dom"
+
+function SearchForm({ searchTerm }) {
   return (
-    <form className="search-form">
-      <input type="search" name="searchTerm" id="searchTerm" required />
-      <button required>Search</button>
-    </form>
+    <Form className="search-form">
+      <input
+        type="search"
+        name="search"
+        id="searchTerm"
+        defaultValue={searchTerm}
+        autoComplete="off"
+        required
+      />
+      <button type="submit" required>
+        Search
+      </button>
+    </Form>
   )
 }
 export default SearchForm

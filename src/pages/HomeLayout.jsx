fb7d9bb7aec9983d11d46ca633ff961bc95f563b
Nicolas Mayorga <namayorgav@gmail.com>
2023-09-30 07:34:54 -0500

    fix: refactored component importing.

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet, useNavigation } from "react-router-dom"
-import Navbar from "../components/navbar"
-import Footer from "../components/footer"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 function HomeLayout() {
   const navigation = useNavigation()

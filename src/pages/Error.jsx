908535ecc803cc989e551c87d6551de062c34268
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-01 21:27:04 -0500

    feat: Error element returning to Landing.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,15 @@
+import { Link } from "react-router-dom"
+import not_found from "../assets/not_found.svg"
+
 function Error() {
-  return <div>Error</div>
+  return (
+    <main className="notFound_container">
+      <img src={not_found} alt="Error not found." />
+      <h3>Could not find what you were looking for ...</h3>
+      <Link to={"/"} className="errorBtn">
+        Go Back Home
+      </Link>
+    </main>
+  )
 }
 export default Error

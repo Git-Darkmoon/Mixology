40f35fcd0f525701d1f1f7b13ea5073a0091c22a
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-01 20:50:21 -0500

    docs: initial structure for the component.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,52 @@
 function About() {
-  return <div>About</div>
+  return (
+    <main className="about">
+      <p>
+        Welcome to Mixology, where the art of crafting the perfect cocktail
+        meets the elegance of intuitive design. I&apos;m delighted to have you
+        here, and as both a marketing supervisor and a UX Designer expert,
+        I&apos;ve strived to make your Mixology experience as delightful as
+        sipping your favorite concoction.
+      </p>
+      <p>
+        At Mixology, we&apos;ve curated an extensive collection of cocktails,
+        each a masterpiece of flavors waiting to be explored. From classic
+        concoctions to innovative mixes, our database is a treasure trove for
+        both seasoned mixologists and those just starting their journey into the
+        world of cocktails.
+      </p>
+      <p>
+        As a UX Designer, my goal is to ensure that navigating through our
+        virtual bar is as smooth as the pour of a fine spirit. The interface is
+        crafted with precision, making it easy for you to discover new recipes,
+        explore ingredients, and elevate your mixology skills. Your journey with
+        Mixology begins with a user experience designed to be not just
+        functional but a pleasure to engage with.
+      </p>
+      <p>
+        Beyond the pixels, Mixology is a celebration of taste and craftsmanship.
+        Each cocktail listed on our platform comes with a carefully detailed
+        recipe, guiding you through the ingredients and preparation process.
+        It&apos;s more than a recipe; it&apos;s an invitation to create, share,
+        and savor moments with friends and loved ones.
+      </p>
+      <p>
+        Join us on this flavorful adventure as we redefine mixology in the
+        digital age. Whether you&apos;re a seasoned bartender or a home
+        enthusiast, Mixology is your go-to destination for all things cocktails.
+        Explore, create, and raise your glass to the exquisite world of
+        Mixology!
+      </p>
+      <p>
+        Cheers,
+        <br />
+        Nicolas Mayorga
+        <br />
+        Systems Engineer and Frontend Developer
+        <br />
+        Mixology
+      </p>
+    </main>
+  )
 }
 export default About
